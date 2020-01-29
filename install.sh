0d26c95a163eec271e721367d098e76516ba90fb
hashuser <60065064+hashuser@users.noreply.github.com>
2020-01-29 16:13:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ conf(){
 }
 
 bbr(){
-  echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+  echo "net.core.default_qdisc=fq" > /etc/sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" > /etc/sysctl.conf
   sysctl -p
 }
 
