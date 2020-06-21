e3f2bf5f59c3d3469ff0fa5fc8e4cc2cb15da215
hashuser <60065064+hashuser@users.noreply.github.com>
2020-06-21 10:13:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ create_shortcut(){
   reboot
 }
 
-system_configr(){
+system_config(){
   echo "net.core.default_qdisc=fq" > /etc/sysctl.conf
   echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
   echo "net.ipv4.tcp_keepalive_time=600" >> /etc/sysctl.conf
