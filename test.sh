9567b8513d2815ce2195d51acdd4a6e9565d3f5c
hashuser <60065064+hashuser@users.noreply.github.com>
2020-12-04 20:29:37 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+port_exist_check(){
+    if [[ 0 -ne `lsof -i:"$1" | wc -l` ]];then
+        lsof -i:"$1" | awk '{print $2}'| grep -v "PID" | xargs kill -9
+}
+
+acme(){
+    port_exist_check 80
+    ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-384 --force
+    if [[ $? -eq 0 ]];then
+        ~/.acme.sh/acme.sh --installcert -d ${domain} --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+}
+
+main(){
+  acme
+}
+
+main
