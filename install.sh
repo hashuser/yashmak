a3ddd1d1a06b26cdbf7beb44155a9dfbc99fc6c5
hashuser <60065064+hashuser@users.noreply.github.com>
2022-01-25 16:07:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,24 +37,24 @@ create_shortcut(){
 }
 
 system_config(){
-  echo "net.core.default_qdisc=fq_codel" > /etc/sysctl.conf
-  echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_keepalive_time=300" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_keepalive_intvl=5" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_keepalive_probes=3" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_moderate_rcvbuf=1" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_window_scaling=1" >> /etc/sysctl.conf
-  echo "net.core.somaxconn=262114" >> /etc/sysctl.conf
-  echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.conf
-  echo "net.ipv4.ip_default_ttl=128" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_adv_win_scale=3" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_max_syn_backlog=8192" >> /etc/sysctl.conf
-  echo "net.nf_conntrack_max=2000000" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_slow_start_after_idle=0" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_fin_timeout=30" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_rmem=4096 65536 12582912" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_wmem=4096 65536 12582912" >> /etc/sysctl.conf
-  echo "net.ipv4.tcp_mem =786432 1048576 26777216" >> /etc/sysctl.conf
+  echo "net.core.default_qdisc = fq_codel" > /etc/sysctl.conf
+  echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_keepalive_time = 300" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_keepalive_intvl = 5" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_keepalive_probes = 3" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_moderate_rcvbuf = 1" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_window_scaling = 1" >> /etc/sysctl.conf
+  echo "net.core.somaxconn = 262114" >> /etc/sysctl.conf
+  echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf
+  echo "net.ipv4.ip_default_ttl = 128" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_adv_win_scale = 3" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_max_syn_backlog = 8192" >> /etc/sysctl.conf
+  echo "net.nf_conntrack_max = 2000000" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_slow_start_after_idle = 0" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_rmem = 4096 65536 12582912" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_wmem = 4096 65536 12582912" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_mem = 786432 1048576 26777216" >> /etc/sysctl.conf
   sysctl -p
   apt-get install resolvconf -y
   echo "dns-nameservers 1.1.1.1" >> /etc/network/interfaces
