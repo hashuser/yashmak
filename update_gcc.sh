0575c5585542ac1bd67cf6240e9ca6ce3b70d7a7
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-02 19:35:40 +0800

    Update update_gcc.sh

--- update_gcc.sh
+++ update_gcc.sh
@@ -7,9 +7,10 @@ main(){
   gcc --version
   wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-$gcc_version.tar.gz
   tar zxf gcc-$gcc_version.tar.gz
+  rm gcc-build
   mkdir gcc-build
   cd gcc-build
-  ../gcc-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++
+  ../gcc-release-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++
   make && sudo make install
   gcc --version
 }
