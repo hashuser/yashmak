7127d92d8c9be4d88a685f553e8f1542c9f83926
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-02 19:40:44 +0800

    Update update_gcc.sh

--- update_gcc.sh
+++ update_gcc.sh
@@ -7,7 +7,7 @@ main(){
   gcc --version
   wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-$gcc_version.tar.gz
   tar zxf gcc-$gcc_version.tar.gz
-  rm gcc-build
+  rm -rf gcc-build
   mkdir gcc-build
   cd gcc-build
   ../gcc-release-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++
