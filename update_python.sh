1102e12b119c2d16c9df0279c6638f5be50c2102
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-02 15:22:14 +0800

    Create update_python.sh

---
+++ update_python.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+python_version="3.9.10"
+
+main(){
+  apt-get update
+  apt-get install libreadline-gplv2-dev
+  apt-get install libncursesw5-dev
+  apt-get install libssl-dev
+  apt-get install libsqlite3-dev tk-dev
+  apt-get install libgdbm-dev
+  apt-get install libbz2-dev
+  apt-get install zlib1g-dev
+  apt-get install libffi-dev
+  apt-get install liblzma-dev
+  apt-get install gcc
+  gcc --version
+  wget https://www.python.org/ftp/python/$python_version/Python-$python_version.tgz
+  tar zxf Python-$python_version.tgz
+  cd Python-$python_version
+  ./configure --prefix=/usr/local/python-$python_version --enable-optimizations
+  make && sudo make install
+}
+
+main
