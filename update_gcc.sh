905a217f191540e121bce6b4f2a037961e2c1b76
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-02 19:45:49 +0800

    Update update_gcc.sh

--- update_gcc.sh
+++ update_gcc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcc_version="11.2.0"
+path=$(cd "$(dirname "$0")";pwd)
 
 main(){
   apt-get update
@@ -8,9 +9,9 @@ main(){
   wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-$gcc_version.tar.gz
   tar zxf gcc-$gcc_version.tar.gz
   rm -rf gcc-build
-  mkdir gcc-build
-  cd gcc-build
-  ../gcc-release-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++
+  mkdir $path/gcc-build
+  cd $path/gcc-build
+  $path/gcc-release-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++
   make && sudo make install
   gcc --version
 }
