68f26ccf04cc0d7ccd75e19a85f59d55ba4d87f6
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-03 21:27:53 +0800

    Update update_gcc.sh

--- update_gcc.sh
+++ update_gcc.sh
@@ -12,9 +12,11 @@ main(){
   apt-get install gcc -y
   apt-get install g++ -y
   gcc --version
+  rm -rf gcc-build
+  rm -rf gcc-releases-gcc-$gcc_version
+  rm -f gcc-$gcc_version.tar.gz
   wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-$gcc_version.tar.gz
   tar zxf gcc-$gcc_version.tar.gz
-  rm -rf gcc-build
   mkdir $path/gcc-build
   cd $path/gcc-build
   $path/gcc-releases-gcc-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++ --disable-multilib
@@ -40,6 +42,8 @@ main(){
   update-alternatives --remove g++ /usr/bin/gcc-9.1.0/bin/g++
   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$gcc_version/bin/gcc 50
   update-alternatives --install /usr/bin/g++ g++ /usr/bin/gcc-$gcc_version/bin/g++ 50
+  rm -rf gcc-build
+  rm -rf gcc-releases-gcc-$gcc_version
 }
 
 main
