1a8a498e5667cd9cba7bbeb797229e45782e91c3
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-03 21:21:29 +0800

    Update update_gcc.sh

--- update_gcc.sh
+++ update_gcc.sh
@@ -20,6 +20,26 @@ main(){
   $path/gcc-releases-gcc-$gcc_version/configure --prefix=/usr/local/gcc-$gcc_version --enable-checking=release --enable-languages=c,c++ --disable-multilib
   make && sudo make install
   gcc --version
+  update-alternatives --remove gcc /usr/bin/gcc-$gcc_version/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-$gcc_version/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-11.1.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-11.1.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-10.3.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-10.3.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-10.2.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-10.2.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-10.1.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-10.1.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-9.4.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-9.4.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-9.3.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-9.3.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-9.2.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-9.2.0/bin/g++
+  update-alternatives --remove gcc /usr/bin/gcc-9.1.0/bin/gcc
+  update-alternatives --remove g++ /usr/bin/gcc-9.1.0/bin/g++
+  update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$gcc_version/bin/gcc 50
+  update-alternatives --install /usr/bin/g++ g++ /usr/bin/gcc-$gcc_version/bin/g++ 50
 }
 
 main
