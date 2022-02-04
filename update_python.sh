b819e6505339ac5f13785132ab3222bcbb9ce430
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-04 11:51:18 +0800

    Update update_python.sh

--- update_python.sh
+++ update_python.sh
@@ -4,6 +4,11 @@ python_version="3.9.10"
 python_main_version="3.9"
 current_python_version=$(python$python_main_version -V)
 current_python_version=${current_python_version: 7}
+cpu_count=$(( $(cat /proc/cpuinfo |grep "processor"|wc -l) - 1 ))
+if [[ $cpu_count < "1" ]]
+then
+        cpu_count="1"
+fi
 
 main(){
 if [[ $current_python_version > $python_version ]] || [[ $current_python_version == $python_version ]]
@@ -25,7 +30,7 @@ else
         tar zxf Python-$python_version.tgz
         cd $path/Python-$python_version
         ./configure --prefix=/usr/local/python-$python_version --enable-optimizations
-        make && sudo make install
+        make -j $cpu_count && make install
         rm /usr/bin/python$python_main_version
         ln -s /usr/local/python-$python_version/bin/python$python_main_version /usr/bin/python$python_main_version
         python3.9 -m pip install dnspython
