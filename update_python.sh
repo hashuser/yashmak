5b99e7af8d96516c82c37af5ab99a5b0a4c9457c
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-06 18:49:14 +0800

    Update update_python.sh

--- update_python.sh
+++ update_python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 path=$(cd "$(dirname "$0")";pwd)
-python_version="3.9.10"
-python_main_version="3.9"
+python_version="3.10.2"
+python_main_version="3.10"
 current_python_version=$(python$python_main_version -V)
 current_python_version=${current_python_version: 7}
 cpu_count=$(( $(cat /proc/cpuinfo |grep "processor"|wc -l) - 1 ))
@@ -33,10 +33,10 @@ else
         make -j $cpu_count && make install
         rm /usr/bin/python$python_main_version
         ln -s /usr/local/python-$python_version/bin/python$python_main_version /usr/bin/python$python_main_version
-        python3.9 -m pip install dnspython
-        python3.9 -m pip install uvloop
-        python3.9 -m pip install ntplib
-        python3.9 -m pip install psutil
+        python$python_main_version -m pip install dnspython
+        python$python_main_version -m pip install uvloop
+        python$python_main_version -m pip install ntplib
+        python$python_main_version -m pip install psutil
         python$python_main_version -V
         systemctl restart Yashmak
         rm -rf $path/Python-$python_version
