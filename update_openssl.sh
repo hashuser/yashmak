a22df34c8dd221640466bf6ea2e61819f201e245
hashuser <60065064+hashuser@users.noreply.github.com>
2022-02-06 18:47:56 +0800

    Update update_openssl.sh

--- update_openssl.sh
+++ update_openssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+openssl_version="1_1_1m"
 path=$(cd "$(dirname "$0")";pwd)
 cpu_count=$(( $(cat /proc/cpuinfo |grep "processor"|wc -l) - 1 ))
 if [[ $cpu_count < "1" ]]
@@ -9,9 +10,9 @@ fi
 main(){
   sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/update_gcc.sh | bash
   update-ca-certificates -f
-  wget https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1l.tar.gz
-  tar xzvf OpenSSL_1_1_1l.tar.gz
-  cd $path/openssl-OpenSSL_1_1_1l
+  wget https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_$openssl_version.tar.gz
+  tar xzvf OpenSSL_$openssl_version.tar.gz
+  cd $path/openssl-OpenSSL_$openssl_version
   ./config
   make -j $cpu_count && make install
   echo "/usr/local/lib" > /etc/ld.so.conf.d/libc.conf
@@ -19,8 +20,8 @@ main(){
   echo "ca_certificate=/etc/ssl/certs/ca-certificates.crt" > /etc/wgetrc
   ldconfig
   openssl version
-  rm -rf $path/openssl-OpenSSL_1_1_1l
-  rm -f $path/OpenSSL_1_1_1l.tar.gz
+  rm -rf $path/openssl-OpenSSL_$openssl_version
+  rm -f $path/OpenSSL_$openssl_version.tar.gz
 }
 
 main
