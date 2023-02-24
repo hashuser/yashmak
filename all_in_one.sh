c4f09b443417fd4504a17f8fd2bb6a1529106c99
hashuser <60065064+hashuser@users.noreply.github.com>
2023-02-24 00:39:42 +0000

    Create all_in_one.sh

---
+++ all_in_one.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+main(){
+  sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/install.sh | bash
+  sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/update_python.sh | bash
+  sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/update_openssl.sh | bash
+}
+
+main
