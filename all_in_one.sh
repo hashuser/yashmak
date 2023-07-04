92e5076e05fd902f64ba1065de5cba363c125e58
hashuser <60065064+hashuser@users.noreply.github.com>
2023-07-04 14:30:34 +0800

    Update all_in_one.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 main(){
+  sudo apt-get install ca-certificates
   sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/update_python.sh | bash
   sudo curl -L https://raw.githubusercontent.com/hashuser/yashmak/master/update_openssl.sh | bash
 }
