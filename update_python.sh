1bc3c73e0610bb862af2332a6f58be7152467628
hashuser <60065064+hashuser@users.noreply.github.com>
2023-07-08 20:59:21 +0800

    Update update_python.sh

--- update_python.sh
+++ update_python.sh
@@ -5,7 +5,7 @@ python_main_version="3.11"
 current_python_version=$(python$python_main_version -V)
 current_python_version=${current_python_version: 7}
 cpu_count=$(( $(cat /proc/cpuinfo |grep "processor"|wc -l) - 1 ))
-if [[ $cpu_count < "1" ]]
+if [[ $cpu_count -lt 1 ]]
 then
         cpu_count="1"
 fi
@@ -50,15 +50,15 @@ rc1_num=$(echo ${array1[2]#$rc1} | grep -o '[0-9]*$' || true)
 array1[2]=$(echo ${array1[2]} | grep -o '^[0-9]*')
 
 # Initial comparison of major, minor, and patch version
-if [[ ${array0[0]} -gt ${array1[0]} ]] || \
-   ([[ ${array0[0]} -eq ${array1[0]} ]] && ([[ ${array0[1]} -gt ${array1[1]} ]] || \
-   ([[ ${array0[1]} -eq ${array1[1]} ]] && [[ ${array0[2]} -gt ${array1[2]} ]]))
+if [[ ${array0[0]} -gt ${array1[0]} ]] || 
+   ([[ ${array0[0]} -eq ${array1[0]} ]] && [[ ${array0[1]} -gt ${array1[1]} ]]) || 
+   ([[ ${array0[0]} -eq ${array1[0]} ]] && [[ ${array0[1]} -eq ${array1[1]} ]] && [[ ${array0[2]} -gt ${array1[2]} ]])
 then
     echo "update required"
     install_python
 else
     # Specific checks for 'rc' versions
-    if [[ ${array0[2]} -eq ${array1[2]} ]] && [[ "$rc0" == "rc" ]] && \
+    if [[ ${array0[2]} -eq ${array1[2]} ]] && [[ "$rc0" == "rc" ]] && 
        ([[ -z "$rc1" ]] || ([[ "$rc1" == "rc" ]] && [[ "$rc0_num" -gt "$rc1_num" ]]))
     then
         echo "update required"
@@ -67,8 +67,6 @@ else
         echo "up to date"
     fi
 fi
-
 }
 
 main
-
