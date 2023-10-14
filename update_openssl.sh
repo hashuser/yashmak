ce36564b46f973292a89040ac95cb27648462960
hashuser <60065064+hashuser@users.noreply.github.com>
2023-10-14 23:27:22 +0100

    Update update_openssl.sh

--- update_openssl.sh
+++ update_openssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl_version="1_1_1u"
+openssl_version="1_1_1w"
 path=$(cd "$(dirname "$0")";pwd)
 cpu_count=$(( $(cat /proc/cpuinfo |grep "processor"|wc -l) - 1 ))
 if [[ $cpu_count < "1" ]]
